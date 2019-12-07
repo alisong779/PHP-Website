f46c5a58f482fa00387956010029eff5a6810ee1
Alison Golden <alisong779@yahoo.com>
2019-12-07 10:57:51 -0500

    move client and server info to footer

--- website/inc/footer.php
+++ website/inc/footer.php
@@ -1,5 +1,30 @@
+<?php include 'server-info.php' ?>
 <footer>
-  <p>Mywebsite &copy; 2019</p>
+  <div class="container">
+  <h1>Server and File Info</h1>
+  <?php if($server): ?>
+    <ul class="list-group">
+      <?php foreach($server as $key => $value): ?>
+        <li class="list-group-item">
+          <strong><?php echo $key; ?>:</strong>
+          <?php echo $value; ?>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  <?php endif; ?>
+  <h1>Client Info</h1>
+  <?php if($client): ?>
+    <ul class="list-group">
+      <?php foreach($client as $key => $value): ?>
+        <li class="list-group-item">
+          <strong><?php echo $key; ?>:</strong>
+          <?php echo $value; ?>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  <?php endif; ?>
+</div>
+<p>Mywebsite &copy; 2019</p>
   </footer>
 </body>
 </html>
