d117ba542947c51469e797285247c5ce4fc61cad
Alison Golden <alisong779@yahoo.com>
2019-12-07 10:51:52 -0500

    add server-info

--- website/inc/header.php
+++ website/inc/header.php
@@ -1,13 +1,28 @@
+<?php include 'server-info.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <title>My Website</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
 </head>
 <body>
+<div class="container">
+  <h1>Server and File Info</h1>
+  <?php if($server): ?>
+    <ul class="list-group">
+      <?php foreach($server as $key => $value): ?>
+        <li class="list-group-item">
+          <strong><?php echo $key; ?>:</strong>
+          <?php echo $value; ?>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  <?php endif; ?>
+</div>
 <ul>
 <li><a href="index.php">Home</a></li>
 <li><a href="about.php">About</a></li>
