95423340363642fefcd18167fde1553a9f8a984c
Alison Golden <alisong779@yahoo.com>
2019-12-07 10:30:32 -0500

    First Commit

---
+++ website/server-info.php
@@ -0,0 +1,13 @@
+<?php
+# $_SERVER SUPERGLOBAL
+
+$server = [
+  'Host Server Name' => $_SERVER['SERVER_NAME'],
+  'Host Header' => $_SERVER['HTTP_HOST'], 
+  'Server Software' => $_SERVER['SERVER_SOFTWARE']
+];
+
+echo $server['Server Software']
+
+
+?>
