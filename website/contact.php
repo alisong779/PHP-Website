4c59817e56aa21dd418999c4659eb5fe54310ed1
Alison Golden <alisong779@yahoo.com>
2019-12-09 11:48:43 -0500

    add contact form conditionals and validations

--- website/contact.php
+++ website/contact.php
@@ -1,7 +1,29 @@
 <?php include 'inc/header.php'; ?>
+<?php
+$msg = '';
+$msgClass = '';
+
+  if(filter_has_var(INPUT_POST, 'submit')){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    if(!empty($email) && !empty($name) && !empty($message)){
+      echo 'Message Sent!';
+    }else {
+      $msg = 'Please fill in all fields';
+      $msgClass = 'alert-danger';
+    }
+  }
+?>
 
 <h1>Contact Me</h1>
-<form action="" method="post">
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+  <?php if($msg != ''): ?>
+    <div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div>
+  <?php endif; ?>
+
+
   <div class="form-group">
       <label>Name</label>
       <input type="text" name="name" class="form-control" placeholder="Your Name">
@@ -14,7 +36,7 @@
     <label>Message</label>
     <textarea name="message" class="form-control" placeholder="Your Message"></textarea>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
 </form>
 
 <?php include 'inc/footer.php'; ?>
