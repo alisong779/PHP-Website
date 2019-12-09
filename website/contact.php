487d1f9bce0c66831e09ed1e8862c61b07c1b141
Alison Golden <alisong779@yahoo.com>
2019-12-09 12:27:14 -0500

    add functions for sending emails

--- website/contact.php
+++ website/contact.php
@@ -18,7 +18,26 @@ $msgClass = '';
         $msg = 'Please enter a valid email';
         $msgClass = 'alert-danger';
       }else {
-        echo 'Submitted';
+        $toEmail = 'agolde726@yahoo.com';
+        $subject = 'Contact Request From '.$name;
+        $body = '<h2>Contact Request</h2>
+        <h4>Name</h4><p>'.$name.'</p>
+        <h4>Email</h4><p>'.$email.'</p>
+        <h4>Message</h4><p>'.$message.'</p>
+        ';
+
+        $headers = "MIME-Version: 1.0" ."/r/n";
+        $headers .= "Content-Type:text/html;charset=UTF-8" . "/r/n";
+        $headers .= "From: " .$name. "<" .$email. ">". "/r/n";
+
+        if(mail($toEmail, $subject, $body, $headers)){
+          $msg = 'Your email has been sent';
+          $msgClass = 'alert-success';
+        }else {
+          $msg = 'Please try again';
+          $msgClass = 'alert-danger';
+        }
+
       }
     }else {
       $msg = 'Please fill in all fields';
